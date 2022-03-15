76bc6395254f6bff8f739be0132c01abd3e57e46
Hallvaeb <hallvard.bjorgen@gmail.com>
2022-03-15 16:35:58 +0100

    Make background fit to window size

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = MyGdxGame.WIDTH;
+		config.height = MyGdxGame.HEIGHT;
+		config.title = MyGdxGame.TITLE;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
