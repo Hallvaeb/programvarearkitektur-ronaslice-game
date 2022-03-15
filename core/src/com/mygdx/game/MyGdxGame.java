5b22809a082d6b1de541db1a2312470797b56c7a
Hallvaeb <hallvard.bjorgen@gmail.com>
2022-03-15 16:22:29 +0100

    add gsm import to remove error

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.states.GameStateManager;
 
 public class MyGdxGame extends ApplicationAdapter {
 
