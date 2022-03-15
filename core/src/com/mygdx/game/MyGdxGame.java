f7dd4bb03ff4717541ad04c5a1ef0dadd0dd1971
madsoe <madsoe@stud.ntnu.no>
2022-03-15 17:39:36 +0100

    menustate og knapp til singleplayer

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,10 +1,13 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.states.GameStateManager;
+import com.mygdx.game.states.MenuState;
 
 public class MyGdxGame extends ApplicationAdapter {
 
@@ -19,15 +22,16 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("bg_bare_himmel.png");
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 1, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
 	
 	@Override
--- core/src/com/mygdx/game/states/MenuState.java
+++ core/src/com/mygdx/game/states/MenuState.java
@@ -1,4 +1,48 @@
 package com.mygdx.game.states;
 
-public class MenuState {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.MyGdxGame;
+
+public class MenuState extends State {
+    private Texture playBtn;
+    private Texture multiBtn;
+    private Texture scoreBtn;
+    private Texture settingBtn;
+    private Texture helpBtn;
+    private Texture img;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        img = new Texture("bg_bare_himmel.png");
+        playBtn = new Texture("heli1.png");
+
+
+    }
+
+    @Override
+    public void handleInput() {
+        if (Gdx.input.justTouched()){
+            gsm.set(new SingleplayerState(gsm));
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInput();
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(img,0, 0);
+        sb.draw(playBtn, (MyGdxGame.WIDTH/2)-(playBtn.getWidth()/2), (MyGdxGame.HEIGHT/2)-(playBtn.getHeight()/2));
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        playBtn.dispose();
+    }
 }
