4758bc824065d91ec7e15de230733235b40ef538
madsoe <madsoe@stud.ntnu.no>
2022-03-15 16:48:55 +0100

    State

---
+++ core/src/com/mygdx/game/states/State.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class State {
+    protected Vector2 mouse;
+    protected GameStateManager gsm;
+
+
+    protected State(GameStateManager gsm){
+        this.gsm = gsm;
+        mouse = new Vector2();
+    }
+
+    protected abstract void handleInput();
+    public abstract void update(float dt);
+    public abstract void render(SpriteBatch sb);
+    public abstract void dispose();
+}
