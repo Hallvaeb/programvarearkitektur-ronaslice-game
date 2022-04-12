bef2fe7c3b257eead90ec6df025476e0f84b6e5e
madsoe <madsoe@stud.ntnu.no>
2022-04-12 15:25:20 +0200

    tom settingState

--- core/src/com/mygdx/game/states/SettingState.java
+++ core/src/com/mygdx/game/states/SettingState.java
@@ -1,13 +1,34 @@
 package com.mygdx.game.states;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SettingState {
+public class SettingState extends State{
     private Texture img;
 
     public SettingState(GameStateManager gsm){
         super(gsm);
         img= new Texture( "bg_bare_himmel.png");
+
+    }
+
+    @java.lang.Override
+    protected void handleInput() {
+
+    }
+
+    @java.lang.Override
+    public void update(float dt) {
+
+    }
+
+    @java.lang.Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @java.lang.Override
+    public void dispose() {
 
     }
 }
